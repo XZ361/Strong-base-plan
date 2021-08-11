e1a71f215162b4ad7ce1a22e039cece0e9f3ffad
王小站 <xiaozhan@wangxiaozhandeMacBook-Air.local>
2021-08-11 11:16:34 +0800

    hello,vue

---
+++ vue-base/hello.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.key">
+            {{item.index}} --{{ item.key }}  --{{item.value}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items:[
+                    {key : 'value1'},
+                    {key : 'value2'},
+                    {key : 'value3'},
+                    {key : 'value4'},
+                ]
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
