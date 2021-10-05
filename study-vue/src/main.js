b836f8cd0620387630386d06d84979a8dff00c5a
XZ361 <2457307263@qq.com>
2021-10-05 16:55:37 +0800

    安装router插件

--- study-vue/src/main.js
+++ study-vue/src/main.js
@@ -1,11 +1,13 @@
 // main是整个项目的单入口文件
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
- // 注册事件总线
+// 注册事件总线
 Vue.prototype.$bus = new Vue();
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
