8a3ede96e9be5455750bfd560200d738d4452c3f
XZ361 <2457307263@qq.com>
2021-10-25 08:14:50 +0800

    监路由的变化

--- study-vue/src/views/Detail.vue
+++ study-vue/src/views/Detail.vue
@@ -7,7 +7,23 @@
 
 <script>
     export default {
-        
+        // created(){  //组件created时，发送一次请求
+        //     console.log('发送详情请求');
+        // }
+        // watch: { //当路径的变化，就发起请求
+        //     $route() {
+        //         console.log('获取数据');
+        //     }
+        // },
+        watch: {//上述程序的简写方式
+            $route: {
+                immediate: true,
+                deep: true,
+                handler() {
+                    console.log('详情获取');
+                }
+            }
+        },
     }
 </script>
 
