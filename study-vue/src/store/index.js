ceda6704bb876f72497a44b3dc441dfc725a0ce3
XZ361 <2457307263@qq.com>
2021-10-30 00:51:45 +0800

    vuex之Actions

--- study-vue/src/store/index.js
+++ study-vue/src/store/index.js
@@ -1,3 +1,4 @@
+// import { resolve } from 'core-js/fn/promise';
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -8,14 +9,49 @@ export default new Vuex.Store({
     isLogin: false
   },
   mutations: {
-    login(state){
+    login(state) {
       state.isLogin = true;
     },
-    logout(state){
+    logout(state) {
       state.isLogin = false;
     }
   },
   actions: {
+    /*
+    *
+    *  Action 类似于 mutation，不同在于：
+    *  Action 提交的是 mutation，而不是直接变更状态。
+    *  Action 可以包含任意异步操作。
+    */
+  //  Action本身不改变状态，只会通过mutations修改状态，简称mutations的搬运工
+  //  参数一是vuex传递的上下文context:{commit,dispatch,state,...}
+  // 里面是store可以调用的一些方法和数据
+    login({ commit }, username) {
+      // 模拟登陆api的调用，1秒钟后，如果判断成功，则登录成功
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (username === 'admin') {
+            commit('login');
+            resolve();
+          } else {
+            reject();
+          }
+        }, 1000);
+      })
+    },
+    logout({ commit }, token) {
+      // 模拟登陆api的调用，1秒钟后，如果判断成功，则登录成功
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (token === 'yes') {
+            commit('logout');
+            resolve();
+          } else {
+            reject();
+          }
+        }, 1000);
+      })
+    }
   },
   modules: {
   }
--- study-vue/src/views/Login.vue
+++ study-vue/src/views/Login.vue
@@ -1,49 +1,64 @@
 <template>
-    <div>
-        <button @click="login" v-if="!isLogin">登录</button>
-        <button @click="logout" v-else>注销</button>
-    </div>
+  <div>
+    <button @click="login" v-if="!isLogin">登录</button>
+    <button @click="logout" v-else>注销</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            login() {
-                // 不能直接变更状态，必须通过提交mutations的方式变更状态
-                this.$store.commit('login');
-                //添加动态路由
-                this.$router.addRoutes([
-                   {
-                        path: '/admin',
-                        name: 'Admin',
-                        component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue'),
-                        children:[
-                            {
-                                path: '/admin/course/:name',
-                                name: 'Detail',
-                                component: () => import('../views/Detail.vue')
-                            }
-                        ],
-                        meta:{
-                            auth: true
-                        }
-                    }
-                ])
-                this.$router.push(this.$route.query.redirect);
+export default {
+  methods: {
+    login() {
+      // 不能直接变更状态，必须通过提交mutations的方式变更状态
+      // this.$store.commit('login');
+      // 派发动作，来触发Actions
+      this.$store
+        .dispatch("login", "admin")
+        .then(() => {
+          //添加动态路由
+          this.$router.addRoutes([
+            {
+              path: "/admin",
+              name: "Admin",
+              component: () =>
+                import(/* webpackChunkName: "about" */ "../views/Admin.vue"),
+              children: [
+                {
+                  path: "/admin/course/:name",
+                  name: "Detail",
+                  component: () => import("../views/Detail.vue"),
+                },
+              ],
+              meta: {
+                auth: true,
+              },
             },
-            logout() {
-                this.$store.commit('logout');
-                this.$router.push('/');
-            }
-        },
-        computed: {
-            isLogin() {
-                return this.$store.state.isLogin; 
-            }
-        },
-    }
+          ]);
+          this.$router.push(this.$route.query.redirect);
+        })
+        .catch(() => {
+          alert("用户名或密码错误");
+        });
+    },
+    logout() {
+      //   this.$store.commit("logout");
+      this.$store
+        .dispatch("logout", "yes")
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(() => {
+          alert("口令错误");
+        });
+    },
+  },
+  computed: {
+    isLogin() {
+      return this.$store.state.isLogin;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
