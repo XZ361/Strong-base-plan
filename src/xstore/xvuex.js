bc0a14cdda054fb55904e36858a4f5f4f1b2bebc
XZ361 <2457307263@qq.com>
2021-12-07 09:13:07 +0800

    注册$store

--- src/xstore/xvuex.js
+++ src/xstore/xvuex.js
@@ -5,13 +5,24 @@
 
 let Vue
 class Store {
-    constructor(){
-        Vue
+    constructor(options) {
+        // 响应式处理的数据:state;通过借鸡生蛋
+        this.state = new Vue({
+            data: options.state
+        })
     }
 }
 
-function install(_Vue){
+function install(_Vue) {
     Vue = _Vue
+    // 注册$store
+    Vue.mixin({
+        beforeCreate() {
+            if (this.$options.store) {
+                Vue.prototype.$store = this.$options.store
+            }
+        }
+    })
 }
 // 现在导出的就是Vuex
-export default {Store,install}
+export default { Store, install }
